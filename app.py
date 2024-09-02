0280b68eff8da43d3083e348501abfbd6dbe6e62
RVVTeles <rafael.teles2@gmail.com>
2024-09-02 16:46:12 -0300

    Teste javascript

--- app.py
+++ app.py
@@ -15,5 +15,10 @@ app.register_blueprint(contas_bp)
 def index():
     return render_template('index.html')
 
+
+@app.route("/test")
+def test():
+    return render_template('test.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
